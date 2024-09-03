c4eca51e2ecc14af635d56fd3d768e7d0c8f6c35
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-09-03 22:47:56 +0530

    server message

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = async (req, res, next) => {
   if (!req.headers.token) {
-    return res.status(400).json({ msg: "Unauthorized" });
+    return res.status(400).json({ message: "Unauthorized" });
   }
 
   jwt.verify(req.headers.token, process.env.JWT_TOKEN, (err, user) => {
-    if (err) return res.status(400).json({ msg: "Unauthorized" });
+    if (err) return res.status(400).json({ message: "Unauthorized" });
     req.body.username = user.username;
     next();
   });
